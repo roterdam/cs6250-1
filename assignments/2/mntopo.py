483ff6618b1c35aacf81232edb2085ab63964980
Harrison Caudill <harrison.caudill@hypersphere.org>
2014-09-03 09:29:11 -0700

    add the turnin version of assignment 2

---
+++ assignments/2/mntopo.py
@@ -0,0 +1,37 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.util import custom
+
+# Topology to be instantiated in Mininet
+class MNTopo(Topo):
+    "Mininet test topology"
+
+    def __init__(self, cpu=.1, max_queue_size=None, **params):
+
+        # Initialize topo
+        Topo.__init__(self, **params)
+
+        # Host and link configuration
+        hostConfig = {'cpu': cpu}
+        linkConfig = {'bw': 50,
+                      'delay': '10ms',
+                      'loss': 0,
+                      'max_queue_size': max_queue_size }
+
+        # Hosts and switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        sender = self.addHost('sender', **hostConfig)
+        receiver = self.addHost('receiver', **hostConfig)
+
+        # Wire sender
+        self.addLink(sender, s1, **linkConfig)
+
+        # Wire receiver
+        self.addLink(receiver, s2, **linkConfig)
+
+        # Connect the dots...I mean switches
+        self.addLink(s1, s2, **linkConfig)
+
+
